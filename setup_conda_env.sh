93da6d0b64c734bd3af6a6ce99ee459fd7b0af4c
carlocorsa <carlo.corsaro@gmail.com>
2020-11-23 00:27:48 +0100

    Fixed typo

--- setup_conda_env.sh
+++ setup_conda_env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 conda create -n calculator python=3.7
-source activate calculator
-conda install flask=1.12
+conda activate calculator
+conda install flask=1.1.2
 conda install parameterized=0.7.4
